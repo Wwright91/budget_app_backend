84ed9ff912e5947d06d1752eac0800c7b62f06e1
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-04 20:52:03 -0400

    GET /transactions/:id
    1. In transactionsController.js implemented route to get a transaction by id

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -6,4 +6,18 @@ router.get("/", (req, res) => {
   res.status(200).send(transactionsArray);
 });
 
+const err = (id) => `Can not find transaction with id: ${id}!`;
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const transaction = transactionsArray.find(
+    (transaction) => transaction.id === id
+  );
+  if (transaction) {
+    res.status(200).send(transaction);
+  } else {
+    res.status(404).json({ error: err(id) });
+  }
+});
+
 module.exports = router;
