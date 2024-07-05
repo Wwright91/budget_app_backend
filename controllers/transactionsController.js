02e33f7a5da813954e23ec72f6ec6771cda2672d
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-05 16:56:07 -0400

    POST /transactions
    1. Installed nanoid
    2. In transactionsController.js implemented the route to create a new transaction

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const transactionsArray = require("../models/transaction");
+const { nanoid } = require("nanoid")
 
 router.get("/", (req, res) => {
   res.status(200).send(transactionsArray);
@@ -19,5 +20,11 @@ router.get("/:id", (req, res) => {
     res.status(404).json({ error: err(id) });
   }
 });
+
+router.post("/", (req, res) => {
+  const currentTransaction = { id: nanoid(2), ...req.body }
+  transactionsArray.push(currentTransaction)
+  res.status(201).send(transactionsArray[transactionsArray.length - 1])
+})
 
 module.exports = router;
