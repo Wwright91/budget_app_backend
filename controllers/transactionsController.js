a1d2fdd3e8126d00282471c82f0b832f6c8e9463
Wwright91 <wisdomwright-martin@pursuit.org>
2024-07-06 10:10:01 -0400

    DELETE /transactions/:id
    1. In transactionsController.js implemented a route to delete a transaction by id

--- controllers/transactionsController.js
+++ controllers/transactionsController.js
@@ -9,6 +9,8 @@ router.get("/", (req, res) => {
 
 const err = (id) => `Can not find transaction with id: ${id}!`;
 
+const getIndex = (id) => transactionsArray.findIndex(transaction => transaction.id === id)
+
 router.get("/:id", (req, res) => {
   const { id } = req.params;
   const transaction = transactionsArray.find(
@@ -25,6 +27,17 @@ router.post("/", (req, res) => {
   const currentTransaction = { id: nanoid(2), ...req.body }
   transactionsArray.push(currentTransaction)
   res.status(201).send(transactionsArray[transactionsArray.length - 1])
+})
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  const deletedTransactionIndex = getIndex(id)
+  if (deletedTransactionIndex !== -1) {
+    transactionsArray.splice(deletedTransactionIndex, 1)
+    res.redirect("/transactions")
+  } else {
+    res.status(404).json({error: err(id)})
+  }
 })
 
 module.exports = router;
